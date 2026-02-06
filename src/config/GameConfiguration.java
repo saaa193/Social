0694560011afeb0982b126b1ea55c2fb9c6738f0
parthipan <parthipan05@icloud.com>
2026-02-06 16:35:05 +0100

    modif GameConfiguration

--- src/config/GameConfiguration.java
+++ src/config/GameConfiguration.java
@@ -1,4 +1,26 @@
 package config;
 
+/**
+ * Copyright SEDAMOP - Software Engineering
+ *
+ * @author tianxiao.liu@cyu.fr
+ *
+ */
 public class GameConfiguration {
+    public static final int WINDOW_WIDTH = 1000;
+    public static final int WINDOW_HEIGHT = 1000;
+
+    public static final int BLOCK_SIZE = 20;
+
+    public static final int LINE_COUNT = WINDOW_HEIGHT / BLOCK_SIZE;
+    public static final int COLUMN_COUNT = WINDOW_WIDTH / BLOCK_SIZE;
+
+    public static final int GAME_SPEED = 1000;
+
+    public static final int MAX_BOMB_COUNT = 3;
+
+    public static final int BOMB_EXPLOSION_DELAY = 3;
+    public static final int NB_HABITANTS = 100;
+
+
 }
